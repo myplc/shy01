2da2494fda1f6dbb9401c23b70f1c09a52278d4a
myplc <plctohmi@gmail.com>
2024-07-10 11:49:20 +0900

    n14수정중

--- n14.js
+++ n14.js
@@ -1,1 +1,19 @@
-// 안녕하세요.
+const express = require("express");
+const logger = require("morgan");
+const fs = require("fs");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("안녕하세요! Express로 만든 서버입니다.");
+});
+
+app.get("/list", (req, res) => {
+  fs.readdir(__dirname, "utf-8", (err, data) => {
+    res.send(data);
+  });
+});
+
+app.listen(port, () => {
+  console.log(port + "에서 서버가 시작되었습니다.");
+});
