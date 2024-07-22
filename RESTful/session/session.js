1b4d5a5cc40ba305ca341c86a5876c9fda636ffa
myplc <plctohmi@gmail.com>
2024-07-22 15:13:46 +0900

    풀이

--- RESTful/session/session.js
+++ RESTful/session/session.js
@@ -33,25 +33,33 @@ app.get("/login", (req, res) => {
 
 app.post("/login", (req, res) => {
   const { username, password } = req.body; // query 는 get 방식
+  const idOK = /^[A-Za-z0-9]{1,8}$/g.test(username); // 방법1. true or false 반환
+  const pwOK = password.match(/^[A-Za-z0-9]{1,8}$/g); // 방법2. 정규표현식에 일치한 값
+  console.log(idOK, pwOK, !!pwOK);
 
-  if (username === "test" && password == "1234") {
-    req.session.loggedIn = true;
-    req.session.username = username;
-    res.redirect("/");
-  } else {
-    res.send(`
+  if (idOK && !!pwOK) {
+    if (username === "test" && password == "1234") {
+      req.session.loggedIn = true;
+      req.session.username = username;
+      res.redirect("/");
+    } else {
+      res.send(`
         <h3>정상적인 로그인이 필요합니다.</h3>
         <button onclick="location.href='/'">뒤로가기</button>
         `);
+    }
+  } else {
+    res.send(`<script>
+      alert('입력조건이 맞지 않습니다. 다시 작성해 주세요!');
+      window.location.href='/login';
+      </script>`);
   }
 });
 
 app.get("/logout", (req, res) => {
   req.session.destroy((e) => {
     if (e) console.error(e);
-    res.send(
-      `<script>alert('로그아웃이 되었습니다!!');window.location.href='/'</script>`
-    );
+    res.send(`<script>alert('로그아웃이 되었습니다!!');window.location.href='/'</script>`);
   });
 });
 
