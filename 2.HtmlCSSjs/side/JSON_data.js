02287d14fd384c6510d346d2c78d2e364b084cc0
myplc <plctohmi@gmail.com>
2024-08-01 09:35:12 +0900

    폴더정리

---
+++ 2.HtmlCSSjs/side/JSON_data.js
@@ -0,0 +1,3 @@
+const obj = { 이름: "홍길동", 나이: 33, 특기: "아버지없음", arr: [1, 2, 3], fn: function () {} };
+// JSON -> { "이름": "홍길동", "나이": 33, "특기": "아버지없음", "arr": [1, 2, 3] }
+console.log(JSON.stringify(obj, null, 2));
